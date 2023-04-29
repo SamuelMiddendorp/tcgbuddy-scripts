f4426f869780d535194d27f8b0feb0f3cad384a9
Samuel Middendorp <samiezaurus@gmail.com>
2023-04-29 20:58:13 +0200

    WIP env variable

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 interface Foo{
     bar: string
 }
+
 const sayBar = (foobar: Foo) => {
     console.log(foobar.bar);
 }
 
 let fooBar = {bar: "bas"};
 
+console.log(process .env.npm_config_port);
 sayBar(fooBar);
+
---
+++ src/model/model.ts
@@ -0,0 +1,24 @@
+export interface Card {
+  _id: string;
+  name: string;
+  supertype: string;
+  subtype?: (string)[] | null;
+  hp: string;
+  types?: (string)[] | null;
+  rules?: null;
+  evolvesTo?: null;
+  attacks?: (AttacksEntity)[] | null;
+  images: Images;
+  functions?: (string)[] | null;
+}
+export interface AttacksEntity {
+  name: string;
+  cost?: (string)[] | null;
+  convertedEnergyCost: number;
+  damage: string;
+  text: string;
+}
+export interface Images {
+  small: string;
+  large: string;
+}
