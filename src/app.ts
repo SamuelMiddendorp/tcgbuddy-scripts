cf1cc8f8e95faaff165fb7d6514f40f6a51e4c3e
Samuel Middendorp <samiezaurus@gmail.com>
2023-08-05 21:46:20 +0200

    Refactor axios request into method

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
-import { time } from "console";
-import { TIMEOUT } from "dns";
-import { write, writeFile, writeFileSync } from "fs";
+import { writeFile, writeFileSync } from "fs";
 import { PokemonSet } from "./model/model";
 
 // Not sure why this is needed, has something to do with the .env not being in the same folder as app.ts
@@ -13,12 +11,25 @@ interface ApiResponse<T> {
 }
 
 const getAllSets = async (): Promise<PokemonSet[]> => {
-    const { data } = await axios.get<ApiResponse<PokemonSet[]>>("https://api.pokemontcg.io/v2/sets", {
-        headers: {
-            "X-Api-Key": userDefinedEnvs.API_KEY
-        }
-    });
-    return data.data;
+
+    const res = await doAxiosRequest<ApiResponse<PokemonSet[]>>(
+        "https://api.pokemontcg.io/v2/sets"
+    )
+    return res.data;
+}
+const doAxiosRequest = async <T,>(url: string): Promise<T> => {
+    try {
+        const res = await axios.get<T>(url, {
+            headers: {
+                "X-Api-Key": userDefinedEnvs.API_KEY
+            }
+        });
+        return res.data;
+    }
+    catch (error) {
+        // We simply log an error and continue executing (hopefully)
+        console.log(Object.keys(error), error.message);
+    }
 }
 const getSetCards = async (setId: string): Promise<any> => {
     let pokemon = [];
@@ -40,20 +51,21 @@ const getSetCards = async (setId: string): Promise<any> => {
     return pokemon;
 }
 const main = async () => {
-
     const sets = await getAllSets();
     writeFile("sets.json", JSON.stringify(sets), (x => x));
-    for(var i = 0; i < sets.length; i++){
+    return;
+    for (var i = 0; i < sets.length; i++) {
         let set = sets[i]
         await timeout(100);
         console.log(`Reading and writing set: ${set.name}`)
         var cards = await getSetCards(set.id);
-        writeFileSync(`foobar/${set.name}-${set.id}.json`, JSON.stringify(cards));
+        writeFileSync(`sets/${set.name}-${set.id}.json`, JSON.stringify(cards));
     }
 }
-function timeout(ms) {
+const timeout = (ms: number) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
 main();
 
 
