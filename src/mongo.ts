faf19cc976355bd348956ea170f750615632995b
Samuel Middendorp <samiezaurus@gmail.com>
2023-08-09 21:19:05 +0200

    append

--- src/mongo.ts
+++ src/mongo.ts
@@ -1,12 +1,12 @@
 import { MongoClient } from "mongodb";
 import { enhance, getCurrentlyAvailableData, log } from "./lib/utils";
 
+
+export const writeCurrentlyAvailableCardsToDb = async () => {
+let collection = db.collection("Cards33")
 let url = "mongodb://127.0.0.1:27017/TCGBUDDY"
 let mongoClient = new MongoClient(url);
 let db = mongoClient.db();
-
-let collection = db.collection("Cards33")
-export const writeCurrentlyAvailableCardsToDb = async () => {
     log("inserting data into mongodb");
     const cards = await getCurrentlyAvailableData();
     for (var i = 0; i < cards.length; i++) {
